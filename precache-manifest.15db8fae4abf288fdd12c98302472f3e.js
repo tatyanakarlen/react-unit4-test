7da4ada2ef8f52bad8954aa43e811f3bb60eaa9d
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-28 20:53:05 -0400

    Updates

---
+++ precache-manifest.15db8fae4abf288fdd12c98302472f3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bb62f5a99b1e72621d7",
+    "url": "/react-unit4-test/static/css/main.e21196b4.chunk.css"
+  },
+  {
+    "revision": "8bb62f5a99b1e72621d7",
+    "url": "/react-unit4-test/static/js/main.8bb62f5a.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/react-unit4-test/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/react-unit4-test/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "0bd2176bec807d972fb0",
+    "url": "/react-unit4-test/static/js/runtime~main.0bd2176b.js"
+  },
+  {
+    "revision": "bc89f2cb000acc49395676bfb568f9cc",
+    "url": "/react-unit4-test/index.html"
+  }
+];
